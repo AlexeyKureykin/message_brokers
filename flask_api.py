7ffac0135cae709c5bb71fc3808503f55fa5ae1e
AlexeyKureykin <59525932+AlexeyKureykin@users.noreply.github.com>
2024-12-02 05:00:38 +0300

    Create flask_api.py

---
+++ flask_api.py
@@ -0,0 +1,23 @@
+import pika
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# Setting up RabbitMQ connection
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
+channel = connection.channel()
+channel.queue_declare(queue='message_queue')
+
+@app.route('/message', methods=['POST'])
+def post_message():
+    data = request.get_json()
+    user = data.get('user', 'anonymous')
+    initial_message = data.get('message', '')
+    message = f"From user: {user}\nMessage: {initial_message}"
+    
+    # Publishing message to RabbitMQ
+    channel.basic_publish(exchange='', routing_key='message_queue', body=message)
+    return jsonify({'status': 'success', 'message': 'Message received'})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
